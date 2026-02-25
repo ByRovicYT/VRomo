363ebda42f20a437342a21e006326f3d34ebf491
ByRovicYT <al22020320@itsa.edu.mx>
2026-02-25 12:12:08 -0600

    Actualizando el mensaje de inicio

--- main.tf
+++ main.tf
@@ -1,45 +1,48 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" # Puedes cambiarla si prefieres otra región
 }
 
 variable "github_token" {
-  description = "Token de acceso personal de GitHub"
+  description = "Token personal de GitHub"
   type        = string
   sensitive   = true
 }
 
-resource "aws_amplify_app" "hola_mundo" {
-  name       = "hola-mundo-vite"
-  repository = "https://github.com/javiercl/vite-hola-mundo.git"
-  oauth_token = var.github_token
+resource "aws_amplify_app" "vite_app" {
+  name       = "VRomo"
+  repository = "https://github.com/ByRovicYT/VRomo.git"
+  # Aquí pasamos el token para que AWS pueda conectarse a GitHub
+  access_token = var.github_token
 
+  # Especificaciones de construcción para un proyecto Vite (Node.js)
   build_spec = <<-EOT
-version: 1
-frontend:
-  phases:
-    preBuild:
-      commands:
-        - npm ci
-    build:
-      commands:
-        - npm run build
-  artifacts:
-    baseDirectory: dist
-    files:
-      - '**/*'
-  cache:
-    paths:
-      - node_modules/**/*
-EOT
-
-  custom_rule {
-    source = "</^((?!\\.).)*$/>"
-    target = "/index.html"
-    status = "200"
-  }
+    version: 1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - npm install
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: dist
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
 }
 
-resource "aws_amplify_branch" "master" {
-  app_id      = aws_amplify_app.hola_mundo.id
-  branch_name = "master"
+# Conectamos la rama 'main' de tu repositorio
+resource "aws_amplify_branch" "main" {
+  app_id            = aws_amplify_app.vite_app.id
+  branch_name       = "main"
+  enable_auto_build = true
+}
+
+# Esto imprimirá la URL de tu aplicación web al terminar
+output "amplify_app_url" {
+  value = "https://${aws_amplify_branch.main.branch_name}.${aws_amplify_app.vite_app.default_domain}"
 }
