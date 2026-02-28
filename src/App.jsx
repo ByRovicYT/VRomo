31957853b079837d8f75cbd8666cf7629281f330
ByRovicYT <al22020320@itsa.edu.mx>
2026-02-27 21:43:13 -0600

    Actualizando el mensaje de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Contador {count}
+          Contador de clicks {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        BIENVENIDOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+        
       </p>
     </>
   )
